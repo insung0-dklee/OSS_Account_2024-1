100c8a0b6ef6fbb37420c59971a4752a291d388f
insung0 <dklee777777@naver.com>
2024-05-29 15:00:23 +0900

    [기능추가] 기능 분리 포멧 추가

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+b_is_exit = 0
+
+while not b_is_exit:
+    func = input("기능 입력 (? 입력시 도움말) : ")
+
+    if func == "1":
+
+        break
+
+    elif func == "2":
+
+        break
+
+    elif func == "3":
+
+        break
+
+    elif func == "?":
+        print("도움말 입력.")
+
+        break
+
+    else:
+        b_is_exit = not b_is_exit
+
