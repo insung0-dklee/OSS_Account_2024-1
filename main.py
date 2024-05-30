d089b300be8f6cf00686a954de959d533f64321c
KimhyeonJi <164855700+rlaguswl04@users.noreply.github.com>
2024-05-30 18:13:05 +0900

    [기능추가] 계산기 기능 추가

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 
 b_is_exit = 0
 
+def calculator():
+    try:
+        # 사용자가 계산할 수식을 입력받는다.
+        expr = input("계산할 수식을 입력하세요 (예: 2 + 3 * 4): ")
+        
+        # eval() 함수를 사용하여 입력된 수식을 평가하고 결과를 result에 저장한다.
+        # eval() 함수는 입력된 문자열을 파이썬 표현식으로 계산해준다.
+        result = eval(expr)
+        
+        # 계산 결과를 출력한다.
+        print(f"결과: {result}")
+    except Exception as e:
+        # 계산 중 오류가 발생하면 예외를 처리하고 오류 메시지를 출력한다.
+        print(f"오류 발생: {e}")
+
 while not b_is_exit:
     func = input("기능 입력 (? 입력시 도움말) : ")
 
@@ -16,6 +31,12 @@ while not b_is_exit:
     elif func == "3":
 
         break
+
+     elif func == "4":
+         calculator() # 함수 calculator를 실행한다.
+
+        
+
 
     elif func == "?":
         print("도움말 입력.")
