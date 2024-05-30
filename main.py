cb0cb38366833e4cc67c9f81e50896a621f96cd4
ChoiJiEun <serp12310229@gmail.com>
2024-05-30 22:14:05 +0900

    [기능 추가] 일자를 지정한 수입/지출 기록 기능, 새 계정의 정보 저장 기능, 저장된 정보를 불러오는 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import hashlib #hashlib 사용
+from datetime import datetime
+import pickle
 
 userdata = {} #아이디, 비밀번호 저장해둘 딕셔너리
 
@@ -18,6 +20,66 @@ def user_reg() : #회원가입
     with open('login.txt', 'a', encoding='UTF-8') as fw: #utf-8 변환 후 login.txt에 작성
         for user_id, user_pw in userdata.items(): #딕셔너리 내에 있는 값을 모두 for문
             fw.write(f'{user_id} : {user_pw}\n') #key, value값을 차례로 login.txt파일에 저장
+
+def day_spending(hist, spending, where="", year=datetime.now().year, month=datetime.now().month, day=datetime.now().day, hour=datetime.now().hour):
+    """
+    일자와 시간을 지정하여 해당 일자의 지출을 dictionary에 리스트 및 튜플 형태로 기록.
+
+    parameters -
+    hist : 기록하고자 하는 dictionary
+    spending : 지출 액수
+    where : 지출 장소, 혹은 지출 이유 등등. 미기재 가능.
+    year, month, day, hour : 지정된 일자의 년, 월, 일. 미기재 가능 (미기재 시 현재의 년월일시로 자동 지정됨)
+    """
+
+    dt = datetime(year, month, day, hour)
+    if f"{dt}" not in hist:     # 해당 일자에 수입지출 내역이 없을 시,
+        hist[f"{dt}"] = []      # 새 리스트 생성
+    hist[f"{dt}"].append((-spending, where))
+
+def day_income(hist, income, where="", year=datetime.now().year, month=datetime.now().month, day=datetime.now().day, hour=datetime.now().hour):
+    """
+    일자와 시간을 지정하여 해당 일자의 수입을 dictionary에 리스트 및 튜플 형태로 기록.
+
+    parameters -
+    hist : 기록하고자 하는 dictionary
+    income : 수입 액수
+    where : 지출 장소, 혹은 지출 이유 등등. 미기재 가능.
+    year, month, day, hour : 지정된 일자의 년, 월, 일. 미기재 가능 (미기재 시 현재의 년월일시로 자동 지정됨)
+    """
+
+    dt = datetime(year, month, day, hour)
+    if f"{dt}" not in hist:     # 해당 일자에 수입지출 내역이 없을 시,
+        hist[f"{dt}"] = []      # 새 리스트 생성
+    hist[f"{dt}"].append((income, where))
+
+def new_account(user_id, bal):
+    """
+    새 계정이 이용될 때, user_id와 bal, history를 포함한 dictionary를 생성하여 저장
+
+    parameters -
+    user_id : 사용자 이름
+    bal : 잔고
+    """
+    household_ledger = {'user_id':user_id, 'bal':bal, 'history':{}}
+
+    with open(f'{user_id}.txt', 'wb') as info:
+        # pickle의 dump 기능을 이용하여 이용자의 이름으로 된 파일에
+        # 이용자의 id, 잔고, 수입/지출 내역(해당 함수 내에서는 초기값 공백)을 저장
+        pickle.dump(household_ledger,info)
+
+def open_account_info(user_id):
+    """
+    user_id의 id를 사용하는 유저의 정보가 저장된 파일을 열어
+    해당 유저의 id, 잔고, 지출/수입 내역이 담긴 dictionary를 return.
+    """
+    try:
+        with open(f'{user_id}.txt', 'rb') as info:
+            user_dict = pickle.load(info)
+        return user_dict
+    except Exception as e:
+        print(f"{user_id}의 정보를 불러오는 과정에서 오류가 발생하였습니다. : {e}")
+        return None
 
 b_is_exit = 0
 
