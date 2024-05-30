b9444d3670c6af02ed4a8ec26e0b83ab2a6957a7
LAPTOP-TLQMBJDL\user <kmw233@gmail.com>
2024-05-30 18:17:59 +0900

    현재 잔액 기능 추가

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
+class HouseholdAccountBook:
+    def __init__(self):
+        self.records = []
+        self.balance = 0
+    
+    def show_balance(self):
+        print(f"현재 잔액: {self.balance}")
 
+def main():
+    book = HouseholdAccountBook()
+    b_is_exit = 0
 
-b_is_exit = 0
+    while not b_is_exit:
+        func = input("기능 입력 (? 입력시 도움말) : ")
 
-while not b_is_exit:
-    func = input("기능 입력 (? 입력시 도움말) : ")
+        if func == "1":
+          pass
+        elif func == "2":
+          pass
+        elif func == "3":
+          pass
+        elif func == "4":
+            book.show_balance()
 
-    if func == "1":
+        elif func == "?":
+            print("4: 현재 잔액 조회")
+            print("5: 종료")
 
-        break
+        elif func == "5":
+            print("프로그램을 종료합니다.")
+            b_is_exit = 1
 
-    elif func == "2":
+        else:
+            print("잘못된 입력입니다. 다시 시도하세요.")
 
-        break
-
-    elif func == "3":
-
-        break
-
-    elif func == "?":
-        print("도움말 입력.")
-
-        break
-
-    else:
-        b_is_exit = not b_is_exit
-
+if __name__ == "__main__":
+    main()
