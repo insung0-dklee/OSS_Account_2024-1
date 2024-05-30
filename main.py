b7d6ab7267a51df4b78fc59284c41bc847e0e552
Yeongi Park <yeongi0111@naver.com>
2024-05-30 18:07:41 +0900

    [기능추가] 지출 계산

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+def minusMoney(origin,money) :
+    return origin - money
+
+
 
 
 b_is_exit = 0
 
 while not b_is_exit:
+    money = 1000;
     func = input("기능 입력 (? 입력시 도움말) : ")
 
     if func == "1":
-
+        deposit = int(input("지출 할 돈을 입력하세요 : "))
+        if deposit >money :
+            print("지출하는 돈이 더 많습니다!")
+            continue;
+        else :
+            money = minusMoney(money,deposit)
+            print("잔고 : " + money)
         break
 
     elif func == "2":
