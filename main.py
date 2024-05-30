8c0f97ec4f506ba1fc0de5cc4ec98fea8a2be277
TaekMin LEE <uh06056@gmail.com>
2024-05-30 18:18:39 +0900

    [기능추가] 상품의 이름과 상품의 가격을 입력하는 프로그램

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-
-
 b_is_exit = 0
-
+Allproduct=[]
+Allcost=[]
+exit=0
 while not b_is_exit:
     func = input("기능 입력 (? 입력시 도움말) : ")
-
+    print("1: 가게부 입력 ")
     if func == "1":
-
+        product=input("무엇을 구매하였는지 작성하시오 : ")
+        cost=int(input("얼마에 구매하였는지 작성하시오 : "))
+        for i in range(0,len(Allproduct)):
+            if Allproduct[i]==product:
+                ALLcost[i]+=cost
+                exit=1
+                break
+        if exit!=1:
+            Allproduct.append(product)
+            Allcost.append(cost) 
         break
 
     elif func == "2":
