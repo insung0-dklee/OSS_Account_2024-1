0bcc108e60198a6eb5276b169a036548417de724
mean-J <165627610+mean-J@users.noreply.github.com>
2024-05-31 01:43:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-def set_total_budget():
-    global total_budget
-	#전역변수로 예산을 지정해 예산 확인 함수에도 사용
-    total_budget = float(input("예산을 입력하세요: "))
-    print(f"예산은 {total_budget}원 입니다.")
-
-def check_total_budget():
-    if total_budget is None:
-        print("예산이 지정되지 않았습니다.")
-    else:
-        print(f"현재 예산은 {total_budget}원 입니다.")
-
-global total_budget
-total_budget = None
-b_is_exit = 0
-=======
 import hashlib #hashlib 사용
 import os
 import json
@@ -265,44 +248,6 @@ def delete_expense():
 b_is_exit = 0
 
 # 메인 루프
->>>>>>> d7b0a1472f482aaff11f5ab8869d9171fb39315b
-while not b_is_exit:
-    func = input("기능 입력 (? 입력시 도움말, 0 입력시 종료) : ")
-
-    if func == "1":
-<<<<<<< HEAD
-
-        pass
-
-    elif func == "2":
-        pass
-
-    elif func == "3":
-         pass
-
-    elif func == "4":
-        # 예산 설정 및 재설정
-        set_total_budget()
-    
-    elif func == "5":
-        # 예산 확인
-        check_total_budget()
-    
-    elif func == "?":
-        print("도움말")
-        print("1: ")
-        print("2: ")
-        print("3: ")
-        print("4: 예산 설정 및 재설정")
-        print("5: 예산 확인")
-        print("0: 종료")
-    
-    elif func == "0":
-        b_is_exit = 1
-    
-    else:
-        print("잘못된 입력입니다. 다시 시도하세요.")
-=======
         add_entry()
     elif func == "2":
         view_entries()
@@ -322,4 +267,3 @@ while not b_is_exit:
         b_is_exit = not b_is_exit
 
         print("올바른 기능을 입력해 주세요.")
->>>>>>> d7b0a1472f482aaff11f5ab8869d9171fb39315b
