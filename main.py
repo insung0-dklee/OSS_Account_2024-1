32b9fee0dc2ddb79b41134285a073ec8e1193667
wlstj <22020306jinseoyeon@gmail.com>
2024-05-30 18:26:28 +0900

    [기능추가] login 기능 구현

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import hashlib
 
+def login():
+    # 사용자로부터 ID 입력받기
+    print("id 입력:", end=" ")
+    id = input()
+
+    # 사용자로부터 비밀번호 입력받기
+    print("password 입력:", end=" ")
+    pw = input()
+
+    # 비밀번호를 SHA-256 해시로 변환
+    h = hashlib.sha256()
+    h.update(pw.encode())  # 입력받은 비밀번호를 인코딩하여 해시 객체에 업데이트
+    pw_data = h.hexdigest()  # 해시 값을 16진수 문자열로 변환
+
+    # 'login.txt' 파일을 읽기 모드로 열기
+    f = open('login.txt', 'r')
 
 b_is_exit = 0
 
@@ -10,7 +27,7 @@ while not b_is_exit:
         break
 
     elif func == "2":
-
+        login()
         break
 
     elif func == "3":
