e550523d7094ef7c95529c329cf4f703ad1630b3
JOWOONG <whdnd5725@gmail.com>
2024-05-30 18:12:03 +0900

    [22112021 조웅] ?입력 시 도움말 기능 추가.

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 
 b_is_exit = 0
 
+def show_help():
+    print("--- 도움말을 출력합니다. ---")
+    print("기능 목록:")
+    print("1: 수입 추가")
+    print("2: 지출 추가")
+    print("3: 거래 내역 보기")
+    print("4: 요약 보기")
+    print("0: 종료")
+
 while not b_is_exit:
     func = input("기능 입력 (? 입력시 도움말) : ")
 
@@ -18,10 +27,8 @@ while not b_is_exit:
         break
 
     elif func == "?":
-        print("도움말 입력.")
-
+        show_help()
         break
-
     else:
         b_is_exit = not b_is_exit
 
