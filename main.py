3b63b054ff1d4f27bbe6db8baf169348d79270ae
Chaeyoon Kim <cy6kimm@gmail.com>
2024-05-30 18:50:16 +0900

    [버그 수정] 프로그램의 종료를 야기하는 break문 뺌

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ while not b_is_exit:
 
     if func == "1":
 
-        break
+        
 
     elif func == "2":
 
-        break
+        
 
     elif func == "3":
 
-        break
+        
 
     elif func == "?":
         print("도움말 입력.")
 
-        break
+        
 
     else:
         b_is_exit = not b_is_exit
