87b60550a10d1526b6b5b5adf867147342132f69
BBangGy <hyeongyu-98@naver.com>
2024-05-30 18:14:19 +0900

    show_help()기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+def show_help():
+    print("도움말:")
+    print("1: 가계부 항목 기입")
+    print("2: 가계부 항목 보기")
+    print("3: 가계부 항목 삭제")
+    print("?: 도움말 보기")
+    print("exit: 프로그램 종료")
+# 기능을 모르는 분들한테 도움을 주기위해 추가했습니다.
 
 b_is_exit = 0
 
@@ -18,8 +25,8 @@ while not b_is_exit:
         break
 
     elif func == "?":
-        print("도움말 입력.")
-
+        show_help()
+        # ?를 입력하면 기능이 무엇인지 알려줍니다.
         break
 
     else:
