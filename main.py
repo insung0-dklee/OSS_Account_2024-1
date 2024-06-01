9ad8c7ff49b6f3cb8d425937543e164f98cd7189
TaekMin LEE <uh06056@gmail.com>
2024-06-02 00:53:17 +0900

    [기능추가] amount를 입력받는 함수 get_valid_amount_input() 함수에서 입력된 숫자가 0부터 시작하는 잘못된 금액이 들어왔을 때 오류를 처리해주는 기능 추가

--- main.py
+++ main.py
@@ -488,10 +488,13 @@ def get_valid_amount_input():
     """
     while True:
         amount = input("금액: ") # 사용자로부터 금액 입력 요청
-        if amount.isdigit(): # 입력이 숫자로만 이루어져 있는지 확인
-            return float(amount) # 숫자로만 이루어져 있다면 입력값을 float로 변환하여 반환
+        if amount[0]=='0': # 금액의 앞자리가 0으로 시작하는 잘못된 입력 방지
+            print("금액을 정확히 입력해주세오")
         else:
-            print("숫자만 입력하세요.") # 입력이 숫자가 아닌 경우, 오류 메시지 출력
+            if amount.isdigit(): # 입력이 숫자로만 이루어져 있는지 확인
+                return float(amount) # 숫자로만 이루어져 있다면 입력값을 float로 변환하여 반환
+            else:
+                print("숫자만 입력하세요.") # 입력이 숫자가 아닌 경우, 오류 메시지 출력
 
 # 수입/지출 항목 추가 함수
 def add_entry():
