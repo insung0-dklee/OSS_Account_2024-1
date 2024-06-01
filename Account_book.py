a209d8c117c61ef42e62c493a6fb3a33210e6d0e
jaaesung <>
2024-06-01 11:45:45 +0900

    [기능추가] GUI구현, main과 account_book 기능 분리

--- Account_book.py
+++ Account_book.py
@@ -1,61 +1,322 @@
-class Account_book: # 가계부 클래스
+import hashlib
+import os
+import json
+from datetime import datetime
+from tkinter import messagebox, simpledialog, ttk, Tk
 
-  bal = 0 #잔액 - 미설정시 0원
-  income_total = 0 #수입 총액
-  income_list = [] #수입 기록 리스트
-  spend_total = 0 #지출 총액
-  spend_list = [] #지출 기록 리스트
-  name = " " #이름 - 미설정시 공백
+userdata = {} #아이디, 비밀번호 저장해둘 딕셔너리
+userphones = {}
+usernames = {}
+userdata2 = {}
+logged_in_user = None
+budget_info = {}
 
-  def __init__(self,name,bal): # 초기화
-    self.name = name #이름 설정
-    if(bal>0):  #잔액 설정
-      self.income_total += bal
-      self.bal = bal
-    else: #잔액이 0원보다 낮으면 0원으로 설정
-      print("금액이 너무 적습니다. 초기값(0원)으로 지정합니다.")
+def user_reg_include_name_phone(): #이름과 전화번호 정보를 포함한 회원가입
+    id = simpledialog.askstring("회원가입", "ID 입력:")
+    if id in userdata:
+        messagebox.showerror("회원가입", "이미 존재하는 ID입니다.")
+        return
+    name = simpledialog.askstring("회원가입", "이름 입력:")
+    phone = simpledialog.askstring("회원가입", "전화번호 입력:")
+    if phone in userphones: #전화번호 중복 체크 - 중복된 전화번호는 가입 불가
+        messagebox.showerror("회원가입", "이미 존재하는 전화번호입니다.")
+        return
+    #비밀번호 입력 시 *로 표시
+    pw = simpledialog.askstring("회원가입", "Password 입력:", show='*')
 
-  def income(self):# 수입 입력시 추가
-    income_money = int(input("수입을 입력하세요 "))
-    if(income_money < 0):#수입 잘못 입력 방지
-      print("0 미만의 값을 입력하셨습니다.")
-      return 0
-    self.bal += income_money #금액 추가 및 내역을 리스트에 넣기
-    self.income_list.append (income_money)
-    self.income_total += income_money
+    h = hashlib.sha256() #hashlib 모듈의 sha256 사용
+    h.update(pw.encode()) #sha256으로 암호화
+    pw_data = h.hexdigest() # 16진수로 변환
 
-  def spend(self):#지출 입력시 추가
-    spend_money = int(input("지출을 입력하세요 "))
-    if(spend_money < 0 or spend_money > self.bal):#지출 잘못 입력 방지
-      print("값을 잘못 입력하셨습니다.")
-      return 0
-    self.bal -= spend_money #금액 감소 및 내역을 리스트에 넣기
-    self.spend_list.append(spend_money)
-    self.spend_total += spend_money
+    userdata[id] = pw_data #아이디와 비밀번호 맵핑
+    userphones[phone] = id #전화번호와 아이디 맵핑
+    usernames[name] = id #이름과 아이디 맵핑
+    userdata2[id] = {'pw': pw_data, 'name': name, 'phone': phone} # key에 id값을, value에 비밀번호와 이름, 전화번호 값
 
-  def show_account(self): #금액 출력
-    print("현재 남은 금액은: ",self.bal, "원 입니다.")
+    with open('login.txt', 'w', encoding='UTF-8') as fw: # utf-8 변환 후 login.txt에 작성
+        for user_id, user_info in userdata2.items(): # 딕셔너리 내에 있는 값을 모두 for문
+            fw.write(f'{user_id} : {user_info["pw"]} : {user_info["name"]} : {user_info["phone"]}\n')  # 아이디, 비밀번호, 이름, 전화번호 값을 차례로 login.txt파일에 저장
 
-  def show_total(self): #수입,지출 총액 출력
-    print("현재까지 소득의 총합은 ",self.income_total,"원 입니다.")
-    print("현재까지 지출의 총합은 ",self.spend_total,"원 입니다.")
+    messagebox.showinfo("회원가입", "회원가입이 완료되었습니다.")
 
-  def show_sortedlist(self): #지출 및 수입 순위 출력
-    print("보고싶은 내역을 선택하세요")
-    button = input("1번 - 수입, 2번 - 지출: ")#수입 혹은 지출 선택
-    if(button == "1"):#주의 - button값은 input으로 받아 char형 변수임
-      print("현재까지의 수입 순위")
-      sortedlist = sorted(self.income_list)[::-1] #수입 리스트 정렬
-      for i in range(0,10):#10위 까지만 출력
-        if(len(self.income_list) <= i):#만약 리스트 크기보다 작을 경우 탈출
-          break
-        print(i+1,"위:",sortedlist[i],"원")
-    elif(button == "2"):
-      print("현재까지 사용한 금액 순위")
-      sortedlist = sorted(self.spend_list)[::-1]#지출 리스트 정렬
-      for i in range(0,10):#10위 까지만 출력
-        if(len(self.income_list) <= i):#만약 리스트 크기보다 작을 경우 탈출
-          break
-        print(i+1,"위 ",sortedlist[i],"원")
+"""
+전화번호를 통해 아이디를 찾는 함수
+"""
+def find_id_by_phone():
+    phone = simpledialog.askstring("아이디 찾기", "찾고자 하는 사용자의 전화번호 입력:")
+    if phone in userphones:
+        messagebox.showinfo("아이디 찾기", f'해당 전화번호로 등록된 아이디는 {userphones[phone]}입니다.')
     else:
-      print("잘못 입력하셨습니다.")
+        messagebox.showerror("아이디 찾기", "해당 전화번호를 가진 사용자가 없습니다.")
+
+def user_login(budget_label):
+    global logged_in_user
+    id = simpledialog.askstring("로그인", "ID 입력:")
+    pw = simpledialog.askstring("로그인", "Password 입력:", show='*')
+
+    h = hashlib.sha256()
+    h.update(pw.encode())
+    pw_data = h.hexdigest()
+
+    if id in userdata and userdata[id] == pw_data:
+        logged_in_user = id
+        load_budget()
+        update_budget_display(budget_label)
+        messagebox.showinfo("로그인", "로그인 성공!")
+    else:
+        messagebox.showerror("로그인", "ID 또는 비밀번호가 잘못되었습니다.")
+
+#로그인을 해야만 가계부 프로그램을 이용할 수 있도록 함수 구현
+def ensure_logged_in(func):
+    def wrapper(*args, **kwargs):
+        if not logged_in_user:
+            messagebox.showerror("오류", "먼저 로그인을 해주세요.")
+        else:
+            return func(*args, **kwargs)
+    return wrapper
+
+"""
+회원 정보를 수정하는 함수
+"""
+@ensure_logged_in
+def modify_user_info():
+    global logged_in_user
+
+    new_phone = simpledialog.askstring("회원 정보 수정", "새로운 전화번호 입력:")
+
+    if new_phone in userphones and userphones[new_phone] != logged_in_user:
+        messagebox.showerror("회원 정보 수정", "이미 등록된 전화번호입니다. 다른 전화번호를 사용해주세요.")
+        return
+
+    new_pw = simpledialog.askstring("회원 정보 수정", "새로운 password 입력:", show='*')
+
+    h = hashlib.sha256()
+    h.update(new_pw.encode())
+    new_pw_data = h.hexdigest()
+
+    # Update user information
+    old_phone = userdata2[logged_in_user]['phone']
+    del userphones[old_phone]
+
+    userdata2[logged_in_user]['pw'] = new_pw_data
+    userdata2[logged_in_user]['phone'] = new_phone
+
+    userphones[new_phone] = logged_in_user
+    userdata[logged_in_user] = new_pw_data
+
+    with open('login.txt', 'w', encoding='UTF-8') as fw:
+        for user_id, user_info in userdata2.items():
+            fw.write(f'{user_id} : {user_info["pw"]} : {user_info["name"]} : {user_info["phone"]}\n')
+
+    messagebox.showinfo("회원 정보 수정", "사용자 정보가 성공적으로 수정되었습니다.")
+
+def save_expense(expense, budget_label):
+    user_expenses_file = f'expenses_{logged_in_user}.json'
+    if not os.path.exists(user_expenses_file):
+        with open(user_expenses_file, 'w') as file:
+            json.dump([], file)
+    with open(user_expenses_file, 'r') as file:
+        data = json.load(file)
+    data.append(expense)
+    with open(user_expenses_file, 'w') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+    update_budget_display(budget_label)
+
+@ensure_logged_in
+def view_expenses():
+    user_expenses_file = f'expenses_{logged_in_user}.json'
+    if not os.path.exists(user_expenses_file):
+        with open(user_expenses_file, 'w') as file:
+            json.dump([], file)
+    with open(user_expenses_file, 'r') as file:
+        data = json.load(file)
+    if data:
+        expenses = "\n".join([f"{idx + 1}. {expense['date']} - {expense['item']} : {expense['amount']}원"
+                              for idx, expense in enumerate(data)])
+        messagebox.showinfo("지출 내역", expenses)
+    else:
+        messagebox.showinfo("지출 내역", "저장된 지출 내역이 없습니다.")
+
+@ensure_logged_in
+def input_expense(budget_label):
+    date = simpledialog.askstring("지출 내역 입력", "지출 날짜 (예: 2024-05-30):")
+    item = simpledialog.askstring("지출 내역 입력", "지출 항목:")
+    amount = simpledialog.askstring("지출 내역 입력", "지출 금액:")
+    if date and item and amount:
+        expense = {'date': date, 'item': item, 'amount': amount}
+        save_expense(expense, budget_label)
+        messagebox.showinfo("지출 내역", "지출 내역이 저장되었습니다.")
+        update_budget_display(budget_label)
+
+@ensure_logged_in
+def delete_expense(budget_label):
+    index = simpledialog.askinteger("지출 내역 삭제", "삭제할 지출 항목의 번호를 입력하세요:")
+    user_expenses_file = f'expenses_{logged_in_user}.json'
+    if not os.path.exists(user_expenses_file):
+        with open(user_expenses_file, 'w') as file:
+            json.dump([], file)
+    with open(user_expenses_file, 'r') as file:
+        data = json.load(file)
+    if index and 1 <= index <= len(data):
+        deleted_expense = data.pop(index - 1)
+        with open(user_expenses_file, 'w') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+        messagebox.showinfo("삭제 완료", f"다음 내역이 삭제되었습니다: {deleted_expense}")
+        update_budget_display(budget_label)
+    else:
+        messagebox.showerror("오류", "잘못된 번호입니다. 다시 시도하세요.")
+
+@ensure_logged_in
+def add_memo():
+    title = simpledialog.askstring("메모장", "메모장 제목:")
+    if title:
+        content = simpledialog.askstring("메모장", "내용 입력:")
+        if content:
+            user_memo_dir = f'memos_{logged_in_user}'
+            if not os.path.exists(user_memo_dir):
+                os.makedirs(user_memo_dir)
+            with open(f"{user_memo_dir}/{title}.txt", "w", encoding="utf8") as new_f:
+                new_f.write(content)
+            messagebox.showinfo("메모장", "메모가 저장되었습니다.")
+        else:
+            messagebox.showerror("메모장", "내용을 입력해주세요.")
+    else:
+        messagebox.showerror("메모장", "제목을 입력해주세요.")
+
+@ensure_logged_in
+def view_memos():
+    user_memo_dir = f'memos_{logged_in_user}'
+    if not os.path.exists(user_memo_dir):
+        os.makedirs(user_memo_dir)
+    
+    memos = [f.split('.')[0] for f in os.listdir(user_memo_dir) if os.path.isfile(os.path.join(user_memo_dir, f))]
+    if memos:
+        memo_title = simpledialog.askstring("메모장", "조회할 메모장 제목을 입력하세요:\n" + "\n".join(memos))
+        if memo_title in memos:
+            with open(f"{user_memo_dir}/{memo_title}.txt", "r", encoding="utf8") as memo_f:
+                memo_content = memo_f.read()
+            messagebox.showinfo(memo_title, memo_content)
+        else:
+            messagebox.showerror("메모장", "해당 제목의 메모장이 없습니다.")
+    else:
+        messagebox.showinfo("메모장", "저장된 메모가 없습니다.")
+
+@ensure_logged_in
+def generate_monthly_report():
+    user_expenses_file = f'expenses_{logged_in_user}.json'
+    if not os.path.exists(user_expenses_file):
+        with open(user_expenses_file, 'w') as file:
+            json.dump([], file)
+    with open(user_expenses_file, 'r') as file:
+        data = json.load(file)
+    
+    if not data:
+        messagebox.showinfo("월별 보고서", "조회할 항목이 없습니다.")
+        return
+    
+    report = {}
+    for expense in data:
+        date = expense['date'][:7]
+        amount = int(expense['amount'])
+        if date not in report:
+            report[date] = 0
+        report[date] += amount
+    
+    report_str = "\n".join([f"{month}: {total}원" for month, total in report.items()])
+    messagebox.showinfo("월별 보고서", report_str)
+
+def load_budget():
+    global budget_info
+    if os.path.exists('budget.json'):
+        with open('budget.json', 'r') as file:
+            budget_info = json.load(file)
+
+def save_budget():
+    with open('budget.json', 'w') as file:
+        json.dump(budget_info, file, ensure_ascii=False, indent=4)
+
+@ensure_logged_in
+def set_budget(budget_label):
+    amount = simpledialog.askinteger("예산 설정", "이번달 예산 금액을 입력하세요:")
+    if amount is not None:
+        budget_info[logged_in_user] = amount
+        save_budget()
+        messagebox.showinfo("예산 설정", f"이번달 예산이 {amount}원으로 설정되었습니다.")
+        update_budget_display(budget_label)
+
+def update_budget_display(budget_label):
+    if logged_in_user:
+        budget = budget_info.get(logged_in_user, 0)
+        expenses = 0
+        user_expenses_file = f'expenses_{logged_in_user}.json'
+        if not os.path.exists(user_expenses_file):
+            with open(user_expenses_file, 'w') as file:
+                json.dump([], file)
+        with open(user_expenses_file, 'r') as file:
+            data = json.load(file)
+        for expense in data:
+            if expense['date'][:7] == datetime.now().strftime('%Y-%m'):
+                expenses += int(expense['amount'])
+        remaining_budget = budget - expenses
+        budget_label.config(text=f"이번달 예산: {budget} 원\n남은 금액: {remaining_budget} 원")
+        if remaining_budget < 0:
+          budget_label.config(text=f"이번달 예산: {budget} 원\n예산 {-1*remaining_budget} 원 초과")
+
+
+
+def calculator():
+    try:
+        expr = simpledialog.askstring("계산기", "계산할 수식을 입력하세요 (예: 2 + 3 * 4):")
+        result = eval(expr)
+        messagebox.showinfo("결과", f"결과: {result}")
+    except Exception as e:
+        messagebox.showerror("오류 발생", f"오류 발생: {e}")
+
+@ensure_logged_in
+def analyze_categories():
+    user_expenses_file = f'expenses_{logged_in_user}.json'
+    if not os.path.exists(user_expenses_file):
+        with open(user_expenses_file, 'w') as file:
+            json.dump([], file)
+    with open(user_expenses_file, 'r') as file:
+        data = json.load(file)
+    
+    if not data:
+        messagebox.showinfo("카테고리 분석", "저장된 지출 내역이 없습니다.")
+        return
+    
+    monthly_categories = {}
+    keywords = {
+        '식비': ['식사', '점심', '저녁', '아침', '간식', '음식', '레스토랑', '카페', '식비'],
+        '교통': ['기름값', '버스', '지하철', '택시', '교통', '주유', '교통비'],
+        '병원' : ['병원', '병원비', '수술'],
+        '여행' : ['해외여행', '항공', '비행기', '숙소', '기차', '항공비']
+    }
+    
+    def get_category(item):
+        for category, words in keywords.items():
+            if any(word in item for word in words):
+                return category
+        return '기타'
+    
+    for expense in data:
+        date = expense['date'][:7]  # Extract year-month
+        item = expense.get('item', '미분류')
+        category = get_category(item)
+        amount = int(expense['amount'])
+        
+        if date not in monthly_categories:
+            monthly_categories[date] = {}
+        
+        if category not in monthly_categories[date]:
+            monthly_categories[date][category] = 0
+        
+        monthly_categories[date][category] += amount
+    
+    report = ""
+    for month, categories in monthly_categories.items():
+        report += f"\n{month}:\n"
+        for category, total in categories.items():
+            report += f"  {category}: {total}원\n"
+    
+    messagebox.showinfo("카테고리 분석", report)
--- main.py
+++ main.py
@@ -1,289 +1,81 @@
-import hashlib #hashlib 사용
-import os
-import json
-from datetime import datetime
-import pickle
+import tkinter as tk
+from tkinter import ttk
 import Account_book
+import os
 
-userdata = {} #아이디, 비밀번호 저장해둘 딕셔너리
+logged_in_user = None
 
-def user_reg() : #회원가입
-    id = input("id 입력: " ) #회원가입 시의 id 입력
+def main():
+    global logged_in_user, root, frame
 
-    pw = input("password 입력: ") #회원가입 시의 pw 입력
+    if os.path.exists('login.txt'):
+        with open('login.txt', 'r', encoding='UTF-8') as fr:
+            for line in fr:
+                id, pw, name, phone = line.strip().split(' : ')
+                Account_book.userdata[id] = pw
+                Account_book.usernames[name] = id
+                Account_book.userphones[phone] = id
+                Account_book.userdata2[id] = {'pw': pw, 'name': name, 'phone': phone}
 
-    h = hashlib.sha256() #hashlib 모듈의 sha256 사용
-    h.update(pw.encode()) #sha256으로 암호화
-    pw_data = h.hexdigest() #16진수로 변환
+    root = tk.Tk()
+    root.title("가계부 프로그램")
+    root.geometry("400x800")
+    root.resizable(False, False)
 
-    f = open('login.txt', 'wb') #login 파일 오픈
+    style = ttk.Style()
+    style.configure('TButton', font=('Helvetica', 12), padding=10)
+    style.configure('TLabel', font=('Helvetica', 14), padding=10)
 
-    userdata[id] = pw_data #key에 id값을, value에 비밀번호 값
+    frame = ttk.Frame(root, padding=20)
+    frame.pack(fill='both', expand=True)
 
-    with open('login.txt', 'a', encoding='UTF-8') as fw: #utf-8 변환 후 login.txt에 작성
-        for user_id, user_pw in userdata.items(): #딕셔너리 내에 있는 값을 모두 for문
-            fw.write(f'{user_id} : {user_pw}\n') #key, value값을 차례로 login.txt파일에 저장
+    update_ui()
 
-def day_spending(hist, spending, where="", year=datetime.now().year, month=datetime.now().month, day=datetime.now().day, hour=datetime.now().hour):
-    """
-    일자와 시간을 지정하여 해당 일자의 지출을 dictionary에 리스트 및 튜플 형태로 기록.
-    parameters -
-    hist : 기록하고자 하는 dictionary
-    spending : 지출 액수
-    where : 지출 장소, 혹은 지출 이유 등등. 미기재 가능.
-    year, month, day, hour : 지정된 일자의 년, 월, 일. 미기재 가능 (미기재 시 현재의 년월일시로 자동 지정됨)
-    """
+    root.mainloop()
 
-    dt = datetime(year, month, day, hour)
-    if f"{dt}" not in hist:     # 해당 일자에 수입지출 내역이 없을 시,
-        hist[f"{dt}"] = []      # 새 리스트 생성
-    hist[f"{dt}"].append((-spending, where))
+def update_ui():
+    global budget_label
 
-def day_income(hist, income, where="", year=datetime.now().year, month=datetime.now().month, day=datetime.now().day, hour=datetime.now().hour):
-    """
-    일자와 시간을 지정하여 해당 일자의 수입을 dictionary에 리스트 및 튜플 형태로 기록.
-    parameters -
-    hist : 기록하고자 하는 dictionary
-    income : 수입 액수
-    where : 지출 장소, 혹은 지출 이유 등등. 미기재 가능.
-    year, month, day, hour : 지정된 일자의 년, 월, 일. 미기재 가능 (미기재 시 현재의 년월일시로 자동 지정됨)
-    """
+    for widget in frame.winfo_children():
+        widget.destroy()
 
-    dt = datetime(year, month, day, hour)
-    if f"{dt}" not in hist:     # 해당 일자에 수입지출 내역이 없을 시,
-        hist[f"{dt}"] = []      # 새 리스트 생성
-    hist[f"{dt}"].append((income, where))
+    ttk.Label(frame, text="가계부 프로그램").pack(pady=10)
+    
+    budget_label = ttk.Label(frame, text="")
+    budget_label.pack(pady=5)
 
-def new_account(user_id, bal):
-    """
-    새 계정이 이용될 때, user_id와 bal, history를 포함한 dictionary를 생성하여 저장
-    parameters -
-    user_id : 사용자 이름
-    bal : 잔고
-    """
-    household_ledger = {'user_id':user_id, 'bal':bal, 'history':{}}
-
-    with open(f'{user_id}.txt', 'wb') as info:
-        # pickle의 dump 기능을 이용하여 이용자의 이름으로 된 파일에
-        # 이용자의 id, 잔고, 수입/지출 내역(해당 함수 내에서는 초기값 공백)을 저장
-        pickle.dump(household_ledger,info)
-
-def open_account_info(user_id):
-    """
-    user_id의 id를 사용하는 유저의 정보가 저장된 파일을 열어
-    해당 유저의 id, 잔고, 지출/수입 내역이 담긴 dictionary를 return.
-    """
-    try:
-        with open(f'{user_id}.txt', 'rb') as info:
-            user_dict = pickle.load(info)
-        return user_dict
-    except Exception as e:
-        print(f"{user_id}의 정보를 불러오는 과정에서 오류가 발생하였습니다. : {e}")
-        return None
-
-def calculator():
-    try:
-        # 사용자가 계산할 수식을 입력받는다.
-        expr = input("계산할 수식을 입력하세요 (예: 2 + 3 * 4): ")
-
-        # eval() 함수를 사용하여 입력된 수식을 평가하고 결과를 result에 저장한다.
-        # eval() 함수는 입력된 문자열을 파이썬 표현식으로 계산해준다.
-        result = eval(expr)
-
-        # 계산 결과를 출력한다.
-        print(f"결과: {result}")
-    except Exception as e:
-        # 계산 중 오류가 발생하면 예외를 처리하고 오류 메시지를 출력한다.
-        print(f"오류 발생: {e}")
-
-# 가계부 데이터 저장 변수
-ledger = []
-
-# 도움말 출력 함수
-def print_help():
-    print("""
-    1: 수입/지출 항목 추가
-    2: 항목 조회
-    3: 월별 보고서 생성
-    4: 예산 설정 및 초과 알림
-    5: 지출 카테고리 분석
-    ?: 도움말 출력
-    exit: 종료
-    """)
-
-# 수입/지출 항목 추가 함수
-def add_entry():
-    date = input("날짜 (YYYY-MM-DD): ")
-    category = input("카테고리: ")
-    description = input("설명: ")
-    amount = float(input("금액: "))
-    entry = {
-        "date": date,
-        "category": category,
-        "description": description,
-        "amount": amount
-    }
-    ledger.append(entry)
-    print("항목이 추가되었습니다.")
-
-# 항목 조회 함수
-def view_entries():
-    for entry in ledger:
-        print(entry)
-
-# 월별 보고서 생성 함수
-def generate_monthly_report():
-    month = input("보고서 생성할 월 (YYYY-MM): ")
-    monthly_total = 0
-    for entry in ledger:
-        if entry["date"].startswith(month):
-            monthly_total += entry["amount"]
-            print(entry)
-    print(f"{month}월 총 지출: {monthly_total} 원")
-
-# 예산 설정 및 초과 알림 함수
-def set_budget():
-    budget = float(input("예산 설정 (원): "))
-    current_total = sum(entry["amount"] for entry in ledger)
-    if current_total > budget:
-        print(f"경고: 예산 초과! 현재 지출: {current_total} 원")
+    if logged_in_user is None:
+        ttk.Button(frame, text="회원가입", command=Account_book.user_reg_include_name_phone).pack(pady=5)
+        ttk.Button(frame, text="로그인", command=login).pack(pady=5)
+        ttk.Button(frame, text="아이디 찾기", command=Account_book.find_id_by_phone).pack(pady=5)
     else:
-        print(f"예산 설정 완료. 현재 지출: {current_total} 원, 남은 예산: {budget - current_total} 원")
+        ttk.Button(frame, text="지출 내역 입력", command=lambda: Account_book.input_expense(budget_label)).pack(pady=5)
+        ttk.Button(frame, text="지출 내역 조회", command=Account_book.view_expenses).pack(pady=5)
+        ttk.Button(frame, text="지출 내역 삭제", command=lambda: Account_book.delete_expense(budget_label)).pack(pady=5)
+        ttk.Button(frame, text="계산기", command=Account_book.calculator).pack(pady=5)
+        ttk.Button(frame, text="메모장 생성", command=Account_book.add_memo).pack(pady=5)
+        ttk.Button(frame, text="메모장 조회", command=Account_book.view_memos).pack(pady=5)
+        ttk.Button(frame, text="월별 보고서 생성", command=Account_book.generate_monthly_report).pack(pady=5)
+        ttk.Button(frame, text="예산 설정", command=lambda: Account_book.set_budget(budget_label)).pack(pady=5)
+        ttk.Button(frame, text="카테고리 분석", command=Account_book.analyze_categories).pack(pady=5)
+        ttk.Button(frame, text="회원 정보 수정", command=Account_book.modify_user_info).pack(pady=5)
+        ttk.Button(frame, text="로그아웃", command=logout).pack(pady=5)
+    
+    if logged_in_user:
+        Account_book.update_budget_display(budget_label)
 
-# 지출 카테고리 분석 함수
-def analyze_categories():
-    category_totals = {}
-    for entry in ledger:
-        category = entry["category"]
-        if category not in category_totals:
-            category_totals[category] = 0
-        category_totals[category] += entry["amount"]
-    for category, total in category_totals.items():
-        print(f"{category}: {total} 원")
+def login():
+    global logged_in_user
+    Account_book.user_login(budget_label)
+    if Account_book.logged_in_user:
+        logged_in_user = Account_book.logged_in_user
+        update_ui()
 
-"""
-add_memo : 파일 입출력을 사용하여 메모장을 추가할 수 있는 기능으로 예상지출내역, 오늘의 목표등을 기록할 수 있다.
-@Parm
-    None
-@Return
-    None
-"""
-def add_memo():
-    print("메모장 제목: ")
-    str_title = input()
-    new_f = open(str_title,"w",encoding="utf8")
-    print("내용 입력: ")
-    str_memo = input()
-    new_f.write(str_memo)
-    new_f.close()
+def logout():
+    global logged_in_user
+    logged_in_user = None
+    Account_book.logged_in_user = None
+    update_ui()
 
-# 지출 내역을 저장할 파일 이름
-expenses_file = 'expenses.json'
-
-# 프로그램 시작 시 파일이 존재하지 않는 경우 초기화
-if not os.path.exists(expenses_file):
-    with open(expenses_file, 'w') as file:
-        json.dump([], file)
-
-def save_expense(expense):
-    # 파일을 열어 기존 데이터를 불러옴
-    with open(expenses_file, 'r') as file:
-        data = json.load(file)
-    # 새 지출 내역을 리스트에 추가
-    data.append(expense)
-    # 데이터를 파일에 저장
-    with open(expenses_file, 'w') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
-
-# 저장된 지출 내역을 조회하는 함수
-def view_expenses():
-    # 파일을 열어 데이터를 불러옴
-    with open(expenses_file, 'r') as file:
-        data = json.load(file)
-        if data:
-            # 데이터가 존재하면 각 지출 내역을 출력
-            for idx, expense in enumerate(data, start=1):
-                print(f"{idx}. {expense['date']} - {expense['item']} : {expense['amount']}원")
-        else:
-            # 데이터가 비어 있으면 해당 메시지 출력
-            print("저장된 지출 내역이 없습니다.")
-
-# 지출 내역을 입력받는 함수
-def input_expense():
-    # 사용자로부터 지출 날짜, 항목, 금액을 입력받음
-    date = input("지출 날짜 (예: 2024-05-30): ")
-    item = input("지출 항목: ")
-    amount = input("지출 금액: ")
-    # 입력받은 데이터를 딕셔너리 형태로 저장
-    expense = {
-        'date': date,
-        'item': item,
-        'amount': amount
-    }
-    # 지출 내역을 파일에 저장
-    save_expense(expense)
-    print("지출 내역이 저장되었습니다.")
-
-# 기능 3: 지출 내역 삭제
-def delete_expense():
-    # 삭제할 지출 항목의 인덱스를 입력받음
-    index = input("삭제할 지출 항목의 번호를 입력하세요: ")
-
-    # 저장된 지출 내역을 불러옴
-    with open(expenses_file, 'r') as file:
-        data = json.load(file)
-    # 입력받은 인덱스가 유효한지 확인하고 삭제
-    try:
-        index = int(index)
-        if 1 <= index <= len(data):
-            deleted_expense = data.pop(index - 1)
-            with open(expenses_file, 'w') as file:
-                json.dump(data, file, ensure_ascii=False, indent=4)
-            print(f"다음 내역이 삭제되었습니다: {deleted_expense}")
-        else:
-            print("잘못된 번호입니다. 다시 시도하세요.")
-    except ValueError:
-        print("숫자를 입력하세요.")
-
-#가계부 초깃값 임의로 설정
-a = Account_book("가계부 1",1000000)
-b = Account_book("가계부 2",2000000)
-c = Account_book("가계부 3",3000000)
-
-Account_list = [a,b,c] #가계부 리스트
-i=0
-
-def choose_Account(func):#가계부 선택 함수
-    print("가계부 선택(번호로 입력)")
-    for i in range(0,len(Account_list)):#가계부 리스트 출력
-      print(f"가계부 {i+1}번 : ",Account_list[i].name)
-    choose = input()
-    return choose 
-
-# 프로그램 종료 여부를 판단하는 변수
-b_is_exit = 0
-
-# 메인 루프
-while not b_is_exit:
-    func = input("기능 입력 (? 입력시 도움말) : ")
-
-    if func == "1":
-        add_entry()
-    elif func == "2":
-        view_entries()
-    elif func == "3":
-        generate_monthly_report()
-    elif func == "4":
-        set_budget()
-    elif func == "5":
-        analyze_categories()
-    elif func == "?":
-        print_help()
-    elif func == "exit":
-        b_is_exit = True
-    elif func == "메모장":
-        add_memo()
-    else:
-        b_is_exit = not b_is_exit
-
-        print("올바른 기능을 입력해 주세요.")
+if __name__ == "__main__":
+    main()
